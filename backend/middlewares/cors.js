ff7a45a280d0b6fbe94b8cdfd14c3c46f8d8dba2
Shnaider Danila <shnd3r@yandex.ru>
2023-09-12 16:35:39 +0700

    feat: Add Access-Control-Allow-Credentials header to ./backend/middlewares/cors.js

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -5,14 +5,15 @@ const cors = (req, res, next) => {
   const { method } = req;
   const requestHeaders = req.headers['access-control-request-headers'];
   if (allowedCors.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', origin)
+    res.header('Access-Control-Allow-Origin', origin);
+    res.header('Access-Control-Allow-Credentials', true);
   }
   if (method === 'OPTIONS') {
-    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS)
-    res.header('Access-Control-Allow-Headers', requestHeaders)
-    return res.end()
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    res.header('Access-Control-Allow-Headers', requestHeaders);
+    return res.end();
   }
-  next()
-}
+  return next();
+};
 
 module.exports = cors;
