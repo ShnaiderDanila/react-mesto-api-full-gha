92b72e19a2f3d95c42fd7e43fa534c5b5e4825d8
Shnaider Danila <shnd3r@yandex.ru>
2023-09-12 20:43:26 +0700

    feat: add SameSite: none and secure: true in login in .backend/controllers/users.js

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -83,6 +83,7 @@ const login = (req, res, next) => {
           maxAge: 3600000 * 24 * 7,
           httpOnly: true,
           sameSite: 'none',
+          secure: true,
         })
         .send({ message: 'Аутентификация успешно выполнена!' });
     })
